c10938d53c2e914eaed7adafc8fa2909a502debe
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2023-06-25 22:33:53 -0300

    feat: finish photo contact component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import {CommonModule} from '@angular/common';
 import {HeaderComponent} from "./components/header/header.component";
 import {RouterLink} from "@angular/router";
 import {ScrollIndicatorComponent} from './components/scroll-indicator/scroll-indicator.component';
-import { PhotoContactsComponent } from './components/photo-contacts/photo-contacts.component';
+import {PhotoContactsComponent} from './components/photo-contacts/photo-contacts.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     ScrollIndicatorComponent,
-    PhotoContactsComponent
+    PhotoContactsComponent,
+    FooterComponent
   ],
   exports: [
     HeaderComponent,
-    ScrollIndicatorComponent
+    ScrollIndicatorComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
